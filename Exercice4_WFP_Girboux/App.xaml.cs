a27819997d6fada55fa2f286fef01b78cf0f631b
Girboux Blaze <230399@CCM-Asty-Moulin.local>
2026-09-16 12:06:43 +0200

    Exercie 1-5 WPF

---
+++ Exercice4_WFP_Girboux/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Exercice4_WFP_Girboux
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
